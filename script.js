0493a86a709e7aa54591888d5c753e99d067d00c
JavierP <jajepelo@gmail.com>
2022-03-10 18:33:39 -0500

    Updated, multiple dots must be fixed

--- script.js
+++ script.js
@@ -2,30 +2,115 @@ const screenNumbers = document.querySelector('.display');
 const buttonsNumber = Array.from(document.querySelectorAll(".number"));
 const buttonsOperator = Array.from(document.querySelectorAll(".operator"));
 const buttonsOperation = [...buttonsNumber, ...buttonsOperator];
+const buttonsClear = document.querySelector(".clear");
+const buttonResult = document.querySelector(".result");
+const buttonDelete= document.querySelector(".delete"); 
 
-
-let selection = []; 
+let selection = [];
 let screenDisplay;
+let lastInput = `${(selection[selection.length - 1])}`;
+let indexSymbol; 
+
+
+
 
 function display() {
     screenDisplay = selection.join("");
     screenNumbers.textContent = screenDisplay
 }
 
-function inputInfo () {
+function clear() {
+    buttonsClear.addEventListener("click", () => {
+        selection = []
+        display()
+    })
+};
+
+function solve() {
+    let partOne = selection.slice (0, indexSymbol).join(""); 
+    let partTwo= selection.slice (indexSymbol+1 ).join("");
+    let numbOne = +partOne
+    let numbTwo= +partTwo
+    let operator = selection[indexSymbol];
+    if (operator == "+" ){
+        let sum = numbOne + numbTwo
+        selection = []
+        selection.push (sum)
+        display(); 
+    }
+    else if (operator == "-" ){
+        let substract = numbOne-numbTwo
+        selection = []
+        selection.push (substract)
+        display(); 
+    } 
+    else if (operator == "x" ){
+        let multiply = numbOne*numbTwo
+        selection = []
+        selection.push (multiply)
+        display(); 
+    } 
+    else if (operator == "/" ){
+        if (numbTwo == 0 ){ 
+            selection=[]
+            selection.push('Error')
+            display()
+            selection= []
+        } else {
+            let divide = numbOne/numbTwo;
+             selection = []; 
+             selection.push (divide)
+             display(); 
+    }
+};
+};
+
+function clickResult (){ 
+    buttonResult.addEventListener ("click", ()=> {
+        solve ()
+    })
+}
+
+function clickDelete (){
+    buttonDelete.addEventListener("click", () => {
+        selection.pop(); 
+        display();
+         
+    }); 
+}; 
+
+function inputInfo() {
     buttonsOperation.forEach(button => {
         button.addEventListener("click", (e) => {
             let inside = e.target.innerText
-            if ( inside == '1' || inside == '2' || inside == '3' || inside == '4' || inside == '5'||  inside == '6 ' || inside == '7' || inside == '9' || inside == '10' || inside == '11'){
+            if (inside == '1' || inside == '2' || inside == '3' || inside == '4' || inside == '5' || inside == '6' || inside == '7' || inside == '8' || inside == '9' || inside == '0' ) {
                 let inputNumberOne = e.target.innerText
                 selection.push(inputNumberOne)
+                lastInput = (selection[selection.length - 1]);
                 display()
-                
             } else {
-                let inputOperator = e.target.innerText
-                selection.push(inputOperator)
-                display()
-                
+                if ((lastInput == "+" || lastInput == "-" || lastInput == "/" || lastInput == "x")) {
+                    inputOperator = e.target.innerText
+                    selection.pop();
+                    selection.push(inputOperator);
+                    lastInput = (selection[selection.length - 1]);
+                    indexSymbol= selection.length - 1
+                    display()
+                } else if (selection.includes('+') || selection.includes('-') || selection.includes('/') || selection.includes('x')) {
+                    solve()
+                    inputOperator = e.target.innerText
+                    selection.push(inputOperator)
+                    lastInput = (selection[selection.length - 1]);
+                    indexSymbol= selection.length - 1
+                    display()
+                } else {
+                    inputOperator = e.target.innerText
+                    selection.push(inputOperator)
+                    lastInput = (selection[selection.length - 1]);
+                    indexSymbol= selection.length - 1
+                    display()
+
+                }
             }
         })
     })
@@ -33,3 +118,6 @@ function inputInfo () {
 
 
 inputInfo();
+clear();
+clickResult();
+clickDelete();
