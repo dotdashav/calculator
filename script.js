506cc4931edd9f86e33fe4df0f91b07ccaf1f1a5
JavierP <jajepelo@gmail.com>
2022-03-09 19:53:22 -0500

    Script added

---
+++ script.js
@@ -0,0 +1,12 @@
+const buttonClear  = document.querySelector ('.clear')
+const buttonPercentage  = document.querySelector  ('.percentage')
+const buttonNegative  =document.querySelector  ('.negative')
+const buttonAdd  = document.querySelector  ('.add')
+const buttonSubstract  =document.querySelector  ('.substract')
+const buttonMultiply  = document.querySelector  ('.multiply')
+const buttonDivide  = document.querySelector  ('.divide') 
+const buttonFloat  = document.querySelector  ('.delete')
+const buttonEqual  = document.querySelector  ('.equal')
+const screenNumbers  = document.querySelector  ('.numbers')
+
+
