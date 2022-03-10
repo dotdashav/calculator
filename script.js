3e0c956de5e809f148bdb1ea215c71269e30f6e3
JavierP <jajepelo@gmail.com>
2022-03-09 21:55:30 -0500

    Js fixed, added function to display inpunt

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const buttonClear  = document.querySelector ('.clear')
-const buttonPercentage  = document.querySelector  ('.percentage')
-const buttonNegative  =document.querySelector  ('.negative')
-const buttonAdd  = document.querySelector  ('.add')
-const buttonSubstract  =document.querySelector  ('.substract')
-const buttonMultiply  = document.querySelector  ('.multiply')
-const buttonDivide  = document.querySelector  ('.divide') 
-const buttonFloat  = document.querySelector  ('.delete')
-const buttonEqual  = document.querySelector  ('.equal')
-const screenNumbers  = document.querySelector  ('.numbers')
+const screenNumbers = document.querySelector('.display');
+const buttonsNumber = Array.from(document.querySelectorAll(".number"));
+const buttonsOperator = Array.from(document.querySelectorAll(".operator"));
+const buttonsOperation = [...buttonsNumber, ...buttonsOperator];
 
 
+let selection = []; 
+let screenDisplay;
+
+function display() {
+    screenDisplay = selection.join("");
+    screenNumbers.textContent = screenDisplay
+}
+
+function inputInfo () {
+    buttonsOperation.forEach(button => {
+        button.addEventListener("click", (e) => {
+            let inside = e.target.innerText
+            if ( inside == '1' || inside == '2' || inside == '3' || inside == '4' || inside == '5'||  inside == '6 ' || inside == '7' || inside == '9' || inside == '10' || inside == '11'){
+                let inputNumberOne = e.target.innerText
+                selection.push(inputNumberOne)
+                display()
+                
+            } else {
+                let inputOperator = e.target.innerText
+                selection.push(inputOperator)
+                display()
+                
+            }
+        })
+    })
+}
+
+
+inputInfo();
